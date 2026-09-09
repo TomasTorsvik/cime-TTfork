5b258d6fe5459d40c7954aff7bdf6e336a0983b7
Tomas Torsvik <tomas.torsvik@uib.no>
2026-09-09 15:11:56 +0200

    Update wget.py following NorESM3

--- scripts/lib/CIME/Servers/wget.py
+++ scripts/lib/CIME/Servers/wget.py
@@ -7,32 +7,69 @@ from CIME.Servers.generic_server import GenericServer
 
 logger = logging.getLogger(__name__)
 
-class WGET(GenericServer):
-    def __init__(self, address, user='', passwd=''):
-        self._args = ''
-        if user:
-            self._args += "--user {}".format(user)
-        if passwd:
-            self._args += "--password {}".format(passwd)
 
-        err = run_cmd("wget {} --spider {}".format(self._args, address))[0]
-        expect(err == 0,"Could not connect to repo '{0}'\nThis is most likely either a proxy, or network issue .")
+class WGET(GenericServer):
+    def __init__(self, address, user="", passwd=""):
+        self._args = "--no-check-certificate "
+        if user:
+            self._args += "--user {} ".format(user)
+        if passwd:
+            self._args += "--password {} ".format(passwd)
         self._server_loc = address
+
+    @classmethod
+    def wget_login(cls, address, user="", passwd=""):
+        args = "--no-check-certificate "
+        if user:
+            args += "--user {} ".format(user)
+        if passwd:
+            args += "--password {} ".format(passwd)
+
+        try:
+            err, _, errstr = run_cmd(
+                "wget {} --spider {}".format(args, address), timeout=60
+            )
+        except RuntimeError:
+            logger.warning(
+                "Could not connect to repo '{0}'\nThis is most likely either a proxy, or network issue .".format(
+                    address
+                )
+            )
+            return None
+        if "Connecting to " in errstr and "... connected" in errstr:
+            logger.warning("Connection established with nonzero code %s", err)
+        elif err:
+            logger.warning(
+                "Could not connect to repo '{0}'\n{1}".format(address, errstr)
+            )
+            return None
+
+        return cls(address, user=user, passwd=passwd)
 
     def fileexists(self, rel_path):
         full_url = os.path.join(self._server_loc, rel_path)
         stat, out, err = run_cmd("wget {} --spider {}".format(self._args, full_url))
-        if (stat != 0):
-            logging.warning("FAIL: Repo '{}' does not have file '{}'\nReason:{}\n{}\n".format(self._server_loc, full_url, out.encode('utf-8'), err.encode('utf-8')))
+
+        if stat != 0:
+            logging.warning(
+                "FAIL: Repo '{}' does not have file '{}'\nReason:{}\n{}\n".format(
+                    self._server_loc, full_url, out, err
+                )
+            )
             return False
         return True
 
     def getfile(self, rel_path, full_path):
         full_url = os.path.join(self._server_loc, rel_path)
-        stat, output, errput = \
-                run_cmd("wget {} {} -nc --output-document {}".format(self._args, full_url, full_path))
-        if (stat != 0):
-            logging.warning("wget failed with output: {} and errput {}\n".format(output, errput))
+        stat, output, errput = run_cmd(
+            "wget {} {} -nc --output-document {}".format(
+                self._args, full_url, full_path
+            )
+        )
+        if stat != 0:
+            logging.warning(
+                "wget failed with output: {} and errput {}\n".format(output, errput)
+            )
             # wget puts an empty file if it fails.
             try:
                 os.remove(full_path)
@@ -45,12 +82,16 @@ class WGET(GenericServer):
 
     def getdirectory(self, rel_path, full_path):
         full_url = os.path.join(self._server_loc, rel_path)
-        stat, output, errput = \
-                run_cmd("wget  {} {} -r -N --no-directories ".format(self._args, full_url+os.sep), from_dir=full_path)
+        stat, output, errput = run_cmd(
+            "wget  {} {} -r -N --no-directories ".format(self._args, full_url + os.sep),
+            from_dir=full_path,
+        )
         logger.debug(output)
         logger.debug(errput)
-        if (stat != 0):
-            logging.warning("wget failed with output: {} and errput {}\n".format(output, errput))
+        if stat != 0:
+            logging.warning(
+                "wget failed with output: {} and errput {}\n".format(output, errput)
+            )
             # wget puts an empty file if it fails.
             try:
                 os.remove(full_path)
